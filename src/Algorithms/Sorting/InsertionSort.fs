ec81669a2c85c4d011bbd711c05327c96c4cf71f
mkowusujr <mrfriiday@gmail.com>
2022-07-18 00:42:57 -0400

    feat: created insertion sort algo

---
+++ src/Algorithms/Sorting/InsertionSort.fs
@@ -0,0 +1,15 @@
+module Algorithms.Sorting
+
+let rec insert (x: 'a) (xs: list<'a>): list<'a> =
+    match (x, xs) with
+    | x, [] -> [x]
+    | x, y::ys -> 
+        if x <= y then
+            x :: y :: ys
+        else
+            y :: insert x ys
+
+let rec isort (list: list<'a>): list<'a> = 
+    match list with
+    | [] -> []
+    | x :: xs -> insert (x) (isort xs)
