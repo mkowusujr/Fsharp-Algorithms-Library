17ae257f6f1ec3da5cba7cea446d1d2c8720bd41
mathew owusu jr <mrfriiday@gmail.com>
2022-07-18 17:31:42 -0400

    added alt factorial func

--- src/Algorithms/Arithmetic/Factorial.fs
+++ src/Algorithms/Arithmetic/Factorial.fs
@@ -3,4 +3,9 @@ module Algorithms.Arithmetic.Factorial
 let rec factorial n =
     match n with
     | 0 -> 1
-    | x ->  factorial (n - 1) * n
+    | x -> factorial (n - 1) * n
+
+let rec factorialAcc n a = 
+    match n with
+    | 0 -> a
+    | x -> factorialAcc (n - 1) (n * a)
--- test/Algorithms.Tests/Arithmetic.Tests/Factorial.Tests.fs
+++ test/Algorithms.Tests/Arithmetic.Tests/Factorial.Tests.fs
@@ -26,3 +26,28 @@ let ``factorial can find the factorial given an input`` () =
     let n7 = 6
     let e7 = 720
     Assert.Equal(e7, factorial n7)
+
+[<Fact>]
+let ``factorialAcc can find the factorial given an input`` () = 
+    let acc = 1
+    let n1 = 0
+    let e1 = 1
+    Assert.Equal(e1, factorialAcc n1 acc)
+    let n2 = 1
+    let e2 = 1
+    Assert.Equal(e2, factorialAcc n2 acc)
+    let n3 = 2
+    let e3 = 2
+    Assert.Equal(e3, factorialAcc n3 acc)
+    let n4 = 3
+    let e4 = 6
+    Assert.Equal(e4, factorialAcc n4 acc)
+    let n5 = 4
+    let e5 = 24
+    Assert.Equal(e5, factorialAcc n5 acc)
+    let n6 = 5
+    let e6 = 120
+    Assert.Equal(e6, factorialAcc n6 acc)
+    let n7 = 6
+    let e7 = 720
+    Assert.Equal(e7, factorialAcc n7 acc)
