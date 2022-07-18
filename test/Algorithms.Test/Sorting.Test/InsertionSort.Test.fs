4a8c90cd4c8dfecd9ff4b0ed172c9f9e60e4b536
mathew owusu jr <mrfriiday@gmail.com>
2022-07-18 14:40:02 -0400

    tested insertion sort

--- test/Algorithms.Test/Sorting.Test/InsertionSort.Test.fs
+++ test/Algorithms.Test/Sorting.Test/InsertionSort.Test.fs
@@ -1,16 +1,35 @@
-module Algorithms.Sorting.Test
+module Algorithms.Sorting.InsertionSort.Tests
 
 open Xunit
-
 open Algorithms.Sorting
 
+let makeRandList count =
+    let rnd = System.Random()
+    List.init count (fun _ -> rnd.Next(1, 100))
+
 [<Fact>]
-let ``insertion sort on [4;3;2;1] yield [1;2;3;4]`` () =
-    let l: list<int> = [4;3;2;1]
-    let expected: list<int> = [1;2;3;4]
-    let actualList: list<int> = Algorithms.Sorting.isort(l)
-    for i in 1 .. (actualList.Lengh - 2 )do
-        Assert.True(actualList[i] < actualList[i+1])
-    // Assert.Equal (isAscending actualList, true)
-    // CollectionAssert.
-    // Assert.Collection(actualList).Equals(expected)
+let ``can makeRandList`` () =
+    let c = 5
+    let l = makeRandList c
+    Assert.Equal(c, l.Length)
+
+[<Fact>]
+let ``insertion sort on reversed list`` () =
+    let l: list<int> = [5;4;3;2;1]
+    let a: list<int> = isort l
+    for i in 1 .. (a.Length - 2 ) do
+        Assert.True(a[i] < a[i+1])
+
+[<Fact>]
+let ``insertion sort on ordered list`` () =
+    let l: list<int> = [1;2;3;4;5]
+    let a: list<int> = isort l
+    for i in 1 .. (a.Length - 2 ) do
+        Assert.True(a[i] < a[i+1])
+
+[<Fact>]
+let ``insertion sort on randomized list`` () =
+    let l: list<int> = makeRandList 15
+    let a: list<int> = isort l
+    for i in 1 .. (a.Length - 2 ) do
+        Assert.True(a[i] <= a[i+1])
