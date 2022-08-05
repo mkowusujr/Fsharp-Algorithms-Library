a5b54be5d2cd64ed2226175a451900e78a71d132
mathew owusu jr <mrfriiday@gmail.com>
2022-08-05 17:27:04 -0400

    finished findMiddleIndex

--- src/Algorithms/Searching/BinarySearch.fs
+++ src/Algorithms/Searching/BinarySearch.fs
@@ -2,10 +2,16 @@ module Algorithms.Searching.BinarySearch
 
 open Algorithms.Arithmetic.BasicArithmetic
 
+let private findMiddleIndex (a: int array): int = 
+    let middleIndex:double = divide (minus (a.Length) (1)) (2)
+    let middleIndexFloor = floor (middleIndex)
+    int middleIndexFloor
+
 let rec searchSlice (a: int array) (value: int): bool =
+    let middleIndex: int = findMiddleIndex a
     match a with
     | [||] -> false
-    | [|_|] -> false
-
-let m (a: int array): int array = 
-    Array.empty
+    | [|_|] ->
+        if value = a[middleIndex] then true
+        elif value < a[middleIndex] then true
+        elif value > a[middleIndex] then true
