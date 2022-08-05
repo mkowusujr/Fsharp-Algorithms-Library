1bc8a100b1c2b310e139fa3d196dfed1e44f8394
mathew owusu jr <mrfriiday@gmail.com>
2022-08-05 16:42:22 -0400

    subbed out binaray serach algo

--- src/Algorithms/Searching/BinarySearch.fs
+++ src/Algorithms/Searching/BinarySearch.fs
@@ -2,3 +2,10 @@ module Algorithms.Searching.BinarySearch
 
 open Algorithms.Arithmetic.BasicArithmetic
 
+let rec searchSlice (a: int array) (value: int): bool =
+    match a with
+    | [||] -> false
+    | [|_|] -> false
+
+let m (a: int array): int array = 
+    Array.empty
