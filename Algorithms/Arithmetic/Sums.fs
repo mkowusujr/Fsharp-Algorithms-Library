59a39640b46598063db97519087c5389eb77f315
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-09-11 00:55:42 -0400

    Updated doc comments

--- Algorithms/Arithmetic/Sums.fs
+++ Algorithms/Arithmetic/Sums.fs
@@ -6,8 +6,8 @@ open Algorithms.Arithmetic.BasicArithmetic
 /// <param name="n">The last number in the sum, must be greater than 1</param>
 let sumTo n = divide (multiply n (add n 1)) 2
 
-/// <summary></summary>
-/// <param name="x"></param>
-/// <param name="n">The last number in the sum, must be greater than 1</param>
+/// <summary>The sum of x to the n</summary>
+/// <param name="x">The base number</param>
+/// <param name="n">The exponent</param>
 let sumOf x n =
     divide (subtract (pown x (add n 1)) 1) (subtract x 1)
