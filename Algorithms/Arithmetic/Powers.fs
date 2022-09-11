68dba13ad0aa55315c314a97cb2345337123788c
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-09-10 23:41:13 -0400

    Added doc comments

--- Algorithms/Arithmetic/Powers.fs
+++ Algorithms/Arithmetic/Powers.fs
@@ -2,12 +2,17 @@ module Algorithms.Arithmetic.Powers
 
 open Algorithms.Arithmetic.BasicArithmetic
 
+/// <summary>Recusively performs exponential math</summary>
+/// <param name="baseNum">The base number</param>
+/// <param name="exponent">The exponent</param>
 let rec raiseToPower baseNum exponent =
     match exponent with
     | 0 -> 1
     | n -> mult (raiseToPower baseNum (minus n 1)) baseNum
 
-
+/// <summary>Recusively performs exponential math with divide and conquor</summary>
+/// <param name="baseNum">The base number</param>
+/// <param name="exponent">The exponent</param>
 let rec raiseToPowerRec baseNum exponent =
     match exponent with
     | 0 -> 1
--- Algorithms/Arithmetic/Sums.fs
+++ Algorithms/Arithmetic/Sums.fs
@@ -2,7 +2,12 @@ module Algorithms.Arithmetic.Sums
 
 open Algorithms.Arithmetic.BasicArithmetic
 
+/// <summary>The sum of numbers from one to n</summary>
+/// <param name="n">The last number in the sum, must be greater than 1</param>
 let sumTo n = divide (mult n (add n 1)) 2
 
+/// <summary></summary>
+/// <param name="x"></param>
+/// <param name="n">The last number in the sum, must be greater than 1</param>
 let sumOf x n =
     divide (minus (pown x (add n 1)) 1) (minus x 1)
