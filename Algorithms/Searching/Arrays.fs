35c9e86d0675af539fbc785cc1dbb791cd19fc4c
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-09-10 22:52:43 -0400

    Removed unneeded comment

--- Algorithms/Searching/Arrays.fs
+++ Algorithms/Searching/Arrays.fs
@@ -1,6 +1,5 @@
 module Algorithms.Searching.Arrays
 
-// Given a collection C of orderable data values and a data value v, is v in C?
 let arraySliceRange (a: int array) (i: int) (j: int) =
     if i > j then Array.empty
     else a[i..j]
